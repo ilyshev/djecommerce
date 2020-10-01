f6d0d4997bcaf17647cb1257613a29a2f6120cc5
ilyshev <dilyshev@mac.com>
2020-09-30 21:00:19 -0300

    Working with home.html, adding items on the page

--- core/views.py
+++ core/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Item
 
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all(),
-    }
-    return render(request, "home-page.html", context)
+# def products(request):
+#     context = {
+#         'items': Item.objects.all(),
+#     }
+#     return render(request, 'products.html', context)
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = 'home.html'
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'product.html'
+
+# def home(request):
+#     context = {
+#         'items': Item.objects.all(),
+#     }
+#     return render(request, 'home.html', context)
